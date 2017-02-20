582800f5cb59b366fac6decedca5c0ae2461f6cd
Gagan Jakhotiya <gagan@coverfoxmail.com>
2017-02-20 13:14:43 +0530

    require imports regex updated

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var Stringify = require('javascript-stringify')
 
 module.exports = function(source){
 	if (source.indexOf("module.exports") !== -1) {
-		var o = "", l = source.match(/(require\(.+\))/g);
+		var o = "", l = source.match(/(require\(["|'][-|_|\w|\d|\.|\/|\\]+\S\))/g);
 		l && l.forEach(function(r, i){
 			o += "var _r" + i + " = " + r + ";\n"
 			source = source.replace(r, '_r' + i)
 		})
 		var d = "(function(){" + source.replace(/(module\.exports)\s*=/, "return ") + "}())";
-		o += "module.exports = eval(" + Stringify(d) + ")";
+		o += "module.exports = eval(" + Stringify(d).replace(/"/g, '\\\'') + ")";
 	}
 	return o || source
 }
